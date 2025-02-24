88161b83f23c17b2255d812e3d75be864cfdcd34
brovkin <andrey.brovkin.1995@mail.ru>
2025-02-24 10:17:43 +0300

    fix for add_product method

--- src/category.py
+++ src/category.py
@@ -17,7 +17,9 @@ class Category:
         Category.total_products += len(products)
 
     def add_product(self, product: Product):
-        """Добавляет продукт в категорию"""
+        """Добавляет продукт в категорию, проверяя его тип"""
+        if not isinstance(product, Product):
+            raise TypeError("Добавляемый объект должен быть экземпляром класса Product")
         self.__products.append(product)
         Category.total_products += 1
 
@@ -28,3 +30,4 @@ class Category:
         for product in self.__products:
             products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
         return products_str.strip()
+
