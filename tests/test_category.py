0a58ebff2283a22aeb47aa645d3bf333e271edd3
brovkin <andrey.brovkin.1995@mail.ru>
2025-02-24 07:17:19 +0300

    fixes, updated tests with fixtures

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,18 +1,27 @@
 from src.category import Category
+from src.product import Product
 
 
-def test_category_of_products(category_of_2_products):
-    category1, category2 = category_of_2_products
+def test_category_initialization(sample_category):
+    assert sample_category.name == "Категория 1"
+    assert sample_category.description == "Описание категории 1"
+    assert len(sample_category.products.splitlines()) == 1
+    assert Category.total_categories == 1
+    assert Category.total_products == 1
 
-    assert category1.name == "2 на выбор"
-    assert category1.description == "Выбирай любое, не ошибешься"
-    assert len(category1.products) == 2
-    assert category1.products[0].name == "Пиво"
-    assert category1.products[1].name == "Кумыс"
 
-    assert category2.name == "1 на выбор"
-    assert category2.description == "Возьми меня"
-    assert len(category2.products) == 1
-    assert category2.products[0].name == "Пиво"
+def test_add_product(sample_category):
+    initial_total_products = Category.total_products
+    new_product = Product(
+        name="Товар 2", description="Описание товара 2", price=200.0, quantity=5
+    )
+    sample_category.add_product(new_product)
 
     assert Category.total_categories == 2
+    assert len(sample_category.products.splitlines()) == 2
+    assert Category.total_products == initial_total_products + 1
+
+
+def test_products_string_representation(sample_category):
+    expected_output = "Товар 1, 100.0 руб. Остаток: 10 шт."
+    assert sample_category.products == expected_output
